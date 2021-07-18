e13014e99c13c80506bd03e9975a624bb724e6c6
Bossologist <85655686+Bossologist@users.noreply.github.com>
2021-07-18 16:20:58 -0700

    Create brute.cpp

---
+++ necklaces/brute.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int N, M;
+	cin >> N >> M;
+	
+	for (int i = 0; i < M; i++)
+	{
+		char c;
+		int a;
+		cin >> c >> a;
+		
+		if (
+	}
+	return 0;
+}
