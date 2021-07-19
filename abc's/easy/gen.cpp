0e5ef1f5da958e681ec86f2d9295a22c6a3d03a6
Bossologist <85655686+Bossologist@users.noreply.github.com>
2021-07-19 08:50:59 -0700

    Update gen.cpp

--- abc's/easy/gen.cpp
+++ abc's/easy/gen.cpp
@@ -13,7 +13,7 @@ int rand(int a, int b)
 }
 
 //easy version
-/*
+
 int main() {
 	srand(time(NULL));
 	ofstream cout;
@@ -36,52 +36,6 @@ int main() {
 		if (i != 1)
 		{
 			test_data += to_string(N) + "\n";
-			string letters[3] = { "A", "B", "C" };
-			for (int i = 0; i < N; i++)
-				test_data += letters[rand(0, 2)];
-		}
-
-		cout.open(to_string(i) + format);
-		if (cout)
-		{
-			cout << test_data << "\n";
-			cout.close();
-		}
-	}
-}
-*/
-
-int main() {
-	srand(time(NULL));
-	ofstream cout;
-
-	for (int i = 1; i <= NUM_CASES; i++)
-	{
-		string format = ".in";
-		string test_data = "";
-		int N = 0, K = 0;
-
-		if (i == 1)
-			test_data = "9 3\nABBCCCBAB";
-		else if (i == 2 || i == 3)
-		{
-			N = rand(20, 100);
-			K = rand(2, N);
-		}
-		else if (i == 8 || i == 10)
-		{
-			N = MAXN;
-			K = rand(10000, 30000);
-		}
-		else
-		{
-			N = rand(90000, MAXN);
-			K = rand(10000, 20000);
-		}
-
-		if (i != 1)
-		{
-			test_data += to_string(N) + " " + to_string(K) + "\n";
 			string letters[3] = { "A", "B", "C" };
 			for (int i = 0; i < N; i++)
 				test_data += letters[rand(0, 2)];
