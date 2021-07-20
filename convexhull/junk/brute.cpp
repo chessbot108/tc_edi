b40cb18bb4fa3a69941d173695911106e1e1c725
chessbot108 <weimingchess108@gmail.com>
2021-07-19 19:53:08 -0700

    more work on convex hull

---
+++ convexhull/junk/brute.cpp
@@ -0,0 +1,96 @@
+#include <set>
+#include <iomanip>
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <algorithm>
+#include <chrono>
+#include <random>
+#define uid(a, b) uniform_int_distribution<int>(a, b)(rng)
+#define ll long long
+#define pb push_back
+#define lb long double
+#define mp make_pair
+using namespace std;
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+struct pt {
+    double x, y;
+};
+
+bool cmp(pt a, pt b) {
+    return a.x < b.x || (a.x == b.x && a.y < b.y);
+}
+
+bool cw(pt a, pt b, pt c) {
+    return a.x*(b.y-c.y)+b.x*(c.y-a.y)+c.x*(a.y-b.y) < 0;
+}
+
+bool ccw(pt a, pt b, pt c) {
+    return a.x*(b.y-c.y)+b.x*(c.y-a.y)+c.x*(a.y-b.y) > 0;
+}
+
+void convex_hull(vector<pt>& a) {
+    if (a.size() == 1)
+        return;
+
+    sort(a.begin(), a.end(), &cmp);
+    pt p1 = a[0], p2 = a.back();
+    vector<pt> up, down;
+    up.push_back(p1);
+    down.push_back(p1);
+    for (int i = 1; i < (int)a.size(); i++) {
+        if (i == a.size() - 1 || cw(p1, a[i], p2)) {
+            while (up.size() >= 2 && !cw(up[up.size()-2], up[up.size()-1], a[i]))
+                up.pop_back();
+            up.push_back(a[i]);
+        }
+        if (i == a.size() - 1 || ccw(p1, a[i], p2)) {
+            while(down.size() >= 2 && !ccw(down[down.size()-2], down[down.size()-1], a[i]))
+                down.pop_back();
+            down.push_back(a[i]);
+        }
+    }
+
+    a.clear();
+    for (int i = 0; i < (int)up.size(); i++)
+        a.push_back(up[i]);
+    for (int i = down.size() - 2; i > 0; i--)
+        a.push_back(down[i]);
+}
+
+
+int main(){
+	cin.tie(0) -> sync_with_stdio(0);
+	//freopen("in.txt", "r", stdin);
+	int T, k;
+	cin >> T >> k;
+	//T = 1000;
+	ll tot = 0, tot1 = 0, cbrt = 0;
+	while(T--){
+		int n;
+		n = k;
+		//cin >> n;
+		multiset<pair<int, int>> ms;
+		cbrt = 0;
+		while(cbrt*cbrt*cbrt < n) cbrt++;
+		vector<pt> ve;
+		for(int i = 0; i<n; i++){
+			int a, b;
+			//cin >> a >> b;
+			a = uid(1, 1e9), b = uid(1, 1e9);
+			while(ms.count(mp(a, b))){
+				a = uid(1, 1e9), b = uid(1, 1e9);
+			}
+			ve.pb({a, b});
+		}
+		convex_hull(ve);
+		tot += (int)(ve.size());
+		tot1 += cbrt;
+		//cout << ve.size() << " " << cbrt << "\n";
+	}
+	lb ra = (lb)tot/(lb)tot1;
+	cout << tot << " " << tot1 << fixed << setprecision(6) << " " << ra << "\n";
+	return 0;
+}
---
+++ convexhull/model.cpp
@@ -0,0 +1,86 @@
+//gyrating cat enthusiast
+#include <iostream>
+#include <cstdio>
+#include <utility>
+#include <cassert>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+
+typedef long double lb;
+typedef long long ll;
+
+const lb eps = 1e-9;
+const ll mod = 1e9 + 7, ll_max = (ll)1e18;
+const int MX = 2e5 +10, int_max = 0x3f3f3f3f;
+
+using namespace std;
+
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+
+//kactl go brrrrrrr
+//https://github.com/kth-competitive-programming/kactl/blob/main/content/geometry/Point.h
+//https://github.com/kth-competitive-programming/kactl/blob/main/content/geometry/ConvexHull.h
+template <class T> int sgn(T x) { return (x > 0) - (x < 0); }
+template<class T>
+struct Point {
+	typedef Point P;
+	T x, y;
+	explicit Point(T x=0, T y=0) : x(x), y(y) {}
+	bool operator<(P p) const { return tie(x,y) < tie(p.x,p.y); }
+	bool operator==(P p) const { return tie(x,y)==tie(p.x,p.y); }
+	P operator+(P p) const { return P(x+p.x, y+p.y); }
+	P operator-(P p) const { return P(x-p.x, y-p.y); }
+	P operator*(T d) const { return P(x*d, y*d); }
+	P operator/(T d) const { return P(x/d, y/d); }
+	T dot(P p) const { return x*p.x + y*p.y; }
+	T cross(P p) const { return x*p.y - y*p.x; }
+	T cross(P a, P b) const { return (a-*this).cross(b-*this); }
+	T dist2() const { return x*x + y*y; }
+	double dist() const { return sqrt((double)dist2()); }
+	// angle to x-axis in interval [-pi, pi]
+	double angle() const { return atan2(y, x); }
+	P unit() const { return *this/dist(); } // makes dist()=1
+	P perp() const { return P(-y, x); } // rotates +90 degrees
+	P normal() const { return perp().unit(); }
+	// returns point rotated 'a' radians ccw around the origin
+	P rotate(double a) const {
+		return P(x*cos(a)-y*sin(a),x*sin(a)+y*cos(a)); }
+	friend ostream& operator<<(ostream& os, P p) {
+		return os << "(" << p.x << "," << p.y << ")"; }
+};
+
+typedef Point<ll> P;
+
+vector<P> convexHull(vector<P> pts) {
+	if (sz(pts) <= 1) return pts;
+	sort(all(pts));
+	vector<P> h(sz(pts)+1);
+	int s = 0, t = 0;
+	for (int it = 2; it--; s = --t, reverse(all(pts)))
+		for (P p : pts) {
+			while (t >= s + 2 && h[t-2].cross(h[t-1], p) <= 0) t--;
+			h[t++] = p;
+		}
+	return {h.begin(), h.begin() + t - (t == 2 && h[0] == h[1])};
+}
+
+//finish copy and paste
+
+vector<P> point;
+
+
+
+int main(){
+  cin.tie(0) -> sync_with_stdio(0);
+
+  return 0;
+}
+
+
