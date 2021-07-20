8b68cdb8ce2fcb4cb645e73062b9f64a47e7137d
Bossologist <85655686+Bossologist@users.noreply.github.com>
2021-07-20 12:22:09 -0700

    Update brute.cpp

--- necklaces/brute.cpp
+++ necklaces/brute.cpp
@@ -1,17 +1,39 @@
 #include <iostream>
+#include <fstream>
+#include <set>
+#include <algorithm>
+#include <string>
 using namespace std;
+#define MAXN 100001
+bool cut[MAXN]; //there is a cut between bead i and i+1
+
+//simplest verison, only calc num elements in link, NlogN cuz sets, could be O(n*inv_ack) dsu easy
 
 int main() {
 	int N, M;
 	cin >> N >> M;
-	
+
+	set<int> b;
+	set<int> e;
+	b.insert(0);
+	e.insert(N);
 	for (int i = 0; i < M; i++)
 	{
 		char c;
 		int a;
 		cin >> c >> a;
-		
-		if (
+
+		if (c == 'R')
+			cut[c] = true;
+
+		if (c == 'Q')
+		{
+			int ans = 0;
+			int n = c;
+			while (n > 0 && !cut[n--]) ans++;
+			n = c;
+			while (n < N && !cut[n++]) ans++;
+		}
 	}
 	return 0;
 }
