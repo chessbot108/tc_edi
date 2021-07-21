6b4508f64183454f1dc9c60cb162486526a9b3df
Bossologist <85655686+Bossologist@users.noreply.github.com>
2021-07-21 12:13:49 -0700

    Add files via upload

---
+++ teleport/bfs.cpp
@@ -0,0 +1,98 @@
+/*
+* bfs with walls and stuff
+* also there are one way teleporters which can chain together
+* N^2
+* medium
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <map>
+#include <queue>
+using namespace std;
+#define MAXN 1000
+string grid[MAXN];
+map<pair<int, int>, pair<int, int> > par;
+int ans[MAXN][MAXN];
+map<pair<int, int>, int> r; //rank for dsu
+bool vis[MAXN][MAXN]; //for bfs
+
+int dx[4] = { 0, 1, 0, -1 };
+int dy[4] = { 1, 0, -1, 0 };
+
+pair<int, int> find(pair<int, int> p)
+{
+	return par[p] = (par[p] == p ? p : find(par[p]));
+}
+
+void merge(pair<int, int> a, pair<int, int> b)
+{
+	a = find(a);
+	b = find(b);
+	if (r[a] > r[b])
+		par[b] = a;
+	else if (r[b] > r[a])
+		par[a] = b;
+	else
+	{
+		par[a] = b;
+		r[b]++;
+	}
+}
+
+int main() {
+	for (int t = 1; t <= 10; t++)
+	{
+		ofstream cout(to_string(t) + ".out");
+		ifstream cin(to_string(t) + ".in");
+
+		int N, M, K;
+		cin >> N >> M >> K;
+
+		for (int i = 0; i < N; i++)
+			cin >> grid[i];
+
+		for (int i = 0; i < N; i++)
+			for (int j = 0; j < M; j++)
+				par[{i, j}] = { i, j };
+
+		for (int i = 0; i < K; i++)
+		{
+			int ax, ay, bx, by;
+			cin >> ax >> ay >> bx >> by;
+
+			par[{--ax, --ay}] = par[{ --bx, --by }];
+		}
+
+		queue<pair<int, int> > q;
+		q.push({ 0, 0 });
+		ans[0][0] = 0;
+		vis[0][0] = true;
+
+		while (q.size())
+		{
+			pair<int, int> curr = q.front();
+			q.pop();
+
+			for (int d = 0; d < 4; d++)
+			{
+				int newx = curr.first + dx[d];
+				int newy = curr.second + dy[d];
+
+				if (newx >= 0 && newx < N && newy >= 0 && newy < N && !vis[newx][newy] && grid[newx][newy] != '#')
+				{
+					pair<int, int> end_pair = find({ newx, newy });
+					vis[end_pair.first][end_pair.second] = true;
+					vis[newx][newy] = true;
+
+					q.push(end_pair);
+					ans[end_pair.first][end_pair.second] = ans[curr.first][curr.second] + 1;
+				}
+			}
+		}
+
+		cout << ans[N - 1][N - 1] << "\n";
+	}
+	return 0;
+}
--- teleport/gen.cpp
+++ teleport/gen.cpp
@@ -1,1 +1,133 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <time.h>
+#include <vector>
+#include <random>
+#include <map>
+#include <chrono>
+using namespace std;
+#define MAXN 1000
+#define MAXK 100000
+#define NUM_CASES 10
+#define WALL_PROB 10 //out of 100
+#define pii pair<int, int>
+map<pii, int> r, group;
+map<pii, pii> par;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 
+pii find(pii n)
+{
+	return (par[n] == n ? n : par[n] = find(par[n]));
+}
+
+void merge(pii a, pii b)
+{
+	a = find(a);
+	b = find(b);
+	if (r[a] > r[b])
+		par[b] = a;
+	else if (r[b] > r[a])
+		par[a] = b;
+	else
+	{
+		par[a] = b;
+		r[b]++;
+	}
+}
+
+//inclusive
+int rand(int a, int b)
+{
+	return (rng() % (b - a + 1) + a);
+}
+
+int main() {
+	srand(time(NULL));
+	ofstream cout;
+
+	for (int i = 1; i <= NUM_CASES; i++)
+	{
+		string format = ".in";
+		string test_data = "";
+		int N = 0, M = 0, K = 0;
+
+		if (i == 1)
+			test_data = "5 5 3\n..#..\n.#...\n...#.\n.##..\n#....\n4 1 5 2\n5 2 4 4\n1 2 5 2";
+		else if (i == 2 || i == 3)
+		{
+			N = rand(10, 50);
+			M = rand(10, 50);
+			K = rand(50, 100);
+		}
+		else if (i == 8 || i == 10)
+		{
+			N = MAXN;
+			M = MAXN;
+			K = MAXK;
+		}
+		else
+		{
+			N = rand(700, MAXN);
+			M = rand(700, MAXN);
+			K = rand(40000, N*M - 9000);
+		}
+
+		vector<pair<int, int> > tel_in, tel_out;
+		if (i != 1)
+		{
+			test_data += to_string(N) + " " + to_string(M) + " " + to_string(K) + "\n";
+			for (int i = 0; i < N; i++)
+			{
+				for (int j = 0; j < M; j++)
+				{
+					group[{ i, j }] = MAXN * i + j;
+					if ((i == 0 && j == 0) || (i == N - 1 && j == M - 1))
+					{
+						test_data += ".";
+						tel_out.push_back({ i + 1, j + 1});
+					}
+					else if (rand(1, 100) <= WALL_PROB) test_data += "#";
+					else
+					{
+						test_data += ".";
+						tel_in.push_back({ i + 1, j + 1 });
+						tel_out.push_back({ i + 1, j + 1 });
+					}
+				}
+				test_data += "\n";
+			}
+
+			shuffle(tel_in.begin(), tel_in.end(), rng);
+
+			for (int i = 0; i < K; i++)
+			{
+				test_data += to_string(tel_in[i].first) + " " + to_string(tel_out[i].second) + " "; //tel start
+				if (i < K / 4)
+				{
+					test_data += to_string(tel_in[i + 1].first) + " " + to_string(tel_out[i + 1].second) + "\n";
+					merge(tel_in[i], tel_in[i + 1]);
+				}
+				else if (i < K / 4 + rand(3, 7))
+					test_data += to_string(N) + " " + to_string(M) + "\n";
+				else
+				{
+					pii out = tel_out[rand(0, tel_out.size() - 1)];
+					while (group[out] == group[tel_in[i]])
+						out = tel_out[rand(0, tel_out.size() - 1)];
+
+					test_data += to_string(out.first) + " " + to_string(out.second);
+					if (i != K - 1) test_data += "\n";
+					merge(tel_in[i], out);
+				}
+			}
+		}
+
+		cout.open(to_string(i) + format);
+		if (cout)
+		{
+			cout << test_data << "\n";
+			cout.close();
+		}
+	}
+}
