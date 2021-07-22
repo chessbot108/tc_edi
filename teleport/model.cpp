c946b16542906d7a297a0660dcbf29f846c94a4f
Bossologist <85655686+Bossologist@users.noreply.github.com>
2021-07-21 20:00:57 -0700

    Update model.cpp

--- teleport/model.cpp
+++ teleport/model.cpp
@@ -11,11 +11,10 @@
 #include <map>
 #include <queue>
 using namespace std;
-#define MAXN 1000
+#define MAXN 1001
 string grid[MAXN];
-map<pair<int, int>, pair<int, int> > par;
+pair<int, int> par[MAXN][MAXN];
 int ans[MAXN][MAXN];
-map<pair<int, int>, int> r; //rank for dsu
 bool vis[MAXN][MAXN]; //for bfs
 
 int dx[4] = { 0, 1, 0, -1 };
@@ -23,57 +22,72 @@ int dy[4] = { 1, 0, -1, 0 };
 
 pair<int, int> find(pair<int, int> p)
 {
-	return par[p] = (par[p] == p ? p : find(par[p]));
+	if (p.first > 1000)
+	{
+		p.first = 1000;
+	}
+	return par[p.first][p.second] = (par[p.first][p.second] == p ? p : find(par[p.first][p.second]));
 }
 
 int main() {
-	int N, M, K;
-	cin >> N >> M >> K;
-
-	for (int i = 0; i < N; i++)
-		cin >> grid[i];
-
-	for (int i = 0; i < N; i++)
-		for (int j = 0; j < M; j++)
-			par[{i, j}] = { i, j };
-
-	for (int i = 0; i < K; i++)
+	for (int t = 1; t <= 10; t++)
 	{
-		int ax, ay, bx, by;
-		cin >> ax >> ay >> bx >> by;
+		ofstream cout(to_string(t) + ".out");
+		ifstream cin(to_string(t) + ".in");
+		memset(par, 0, sizeof par);
+		memset(vis, false, sizeof vis);
+		memset(ans, 0, sizeof ans);
 
-		par[{--ax, --ay}] = par[{ --bx, --by }];
-	}
-		
-	for (auto p : par)
-		find(p.first);
+		int N, M, K;
+		cin >> N >> M >> K;
 
-	queue<pair<int, int> > q;
-	q.push({ 0, 0 });
-	ans[0][0] = 0;
-	vis[0][0] = true;
-	while (q.size())
-	{
-		pair<int, int> curr = q.front();
-		q.pop();
-		vis[curr.first][curr.second] = true;
+		for (int i = 0; i < N; i++)
+			cin >> grid[i];
 
-		for (int d = 0; d < 4; d++)
+		for (int i = 0; i < N; i++)
+			for (int j = 0; j < M; j++)
+				par[i][j] = { i, j };
+
+		for (int i = 0; i < K; i++)
 		{
-			int newx = curr.first + dx[d];
-			int newy = curr.second + dy[d];
+			int ax, ay, bx, by;
+			cin >> ax >> ay >> bx >> by;
 
-			pair<int, int> end_pair = find({ newx, newy });
-			if (newx >= 0 && newx < N && newy >= 0 && newy < M && grid[newx][newy] != '#' && !vis[end_pair.first][end_pair.second])
+			par[--ax][--ay] = par[--bx][--by];
+		}
+		
+		for (int i = 0; i < N; i++)
+			for (int j = 0; j < M; j++)
+				find({ i, j });
+
+		queue<pair<int, int> > q;
+		q.push({ 0, 0 });
+		ans[0][0] = 0;
+		vis[0][0] = true;
+
+		while (q.size())
+		{
+			pair<int, int> curr = q.front();
+			q.pop();
+			vis[curr.first][curr.second] = true;
+
+			for (int d = 0; d < 4; d++)
 			{
-				vis[end_pair.first][end_pair.second] = true;
+				int newx = curr.first + dx[d];
+				int newy = curr.second + dy[d];
 
-				q.push(end_pair);
-				ans[end_pair.first][end_pair.second] = ans[curr.first][curr.second] + 1;
+				pair<int, int> end_pair = find({ newx, newy });
+				if (newx >= 0 && newx < N && newy >= 0 && newy < M && grid[newx][newy] != '#' && !vis[end_pair.first][end_pair.second])
+				{
+					vis[end_pair.first][end_pair.second] = true;
+
+					q.push(end_pair);
+					ans[end_pair.first][end_pair.second] = ans[curr.first][curr.second] + 1;
+				}
 			}
 		}
-	}
 
-	cout << ans[N - 1][M - 1] << "\n";
+		cout << ans[N - 1][M - 1] << "\n";
+	}
 	return 0;
 }
