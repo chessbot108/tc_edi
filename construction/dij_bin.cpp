6964c5cd0956062c71ec401f7a66e69a52f4dd89
Bossologist <85655686+Bossologist@users.noreply.github.com>
2021-07-29 11:16:27 -0700

    Update dij_bin.cpp

--- construction/dij_bin.cpp
+++ construction/dij_bin.cpp
@@ -14,17 +14,22 @@
 #include <set>
 #include <algorithm>
 #include <string>
+#include <queue>
+#include <memory.h>
 using namespace std;
-#define MAXN 50001
+#define MAXN 20001
+#define MAX_INT 2000000000
 vector<int> adj[MAXN];
+pair<int, int> removed = { -1, -1 };
 map<pair<int, int>, int> wt, flow, ind;
 map<pair<int, int>, bool> is_path;
 vector<pair<int, int> > edges, path;
-int dist[MAXN], end_dist[MAXN], par[MAXN], npar[MAXN], st[4*MAXN], llast[MAXN], rfirst[MAXN];
+int dist[MAXN], end_dist[MAXN], par[MAXN], npar[MAXN], st[4*MAXN], llast[2*MAXN], rfirst[2*MAXN];
 int N, M, K;
 
 void update(int l, int r, int ul, int ur, int si, int val)
 {
+	if (ur < ul) return;
 	if (ul <= l && r <= ur)
 	{
 		st[si] = min(st[si], val);
@@ -42,30 +47,31 @@ int query(int l, int r, int si, int e)
 	if (l == r) return st[si];
 
 	int m = (l + r) / 2;
-	return min(st[si], e < m ? query(l, m, 2 * si + 1, e) : query(m + 1, r, 2 * si + 2, e));
+	return min(st[si], e <= m ? query(l, m, 2 * si + 1, e) : query(m + 1, r, 2 * si + 2, e));
 }
 
 bool init_dij(int min_flow, int n)
 {
-	set<pair<int, int> > vis;
+	priority_queue<pair<int, int> > vis;
 	end_dist[n] = 0;
-	vis.insert({ 0, n });
+	vis.push({ 0, n });
 
 	while (vis.size())
 	{
-		int curr = vis.begin()->second;
-		int distance = vis.begin()->first;
-		vis.erase(vis.begin());
+		int curr = vis.top().second;
+		int distance = -vis.top().first;
+		vis.pop();
+
 		if (!curr && distance <= K) return true;
-		else if (!curr) continue;
+		else if (!curr) return false;
 
 		for (int c : adj[curr])
 		{
 			if (c == par[curr]) continue;
-			if ((end_dist[c] == 33686018 || end_dist[curr] + wt[{curr, c}] < end_dist[c]) && flow[{curr, c}] <= min_flow)
+			if ((end_dist[c] == MAX_INT || end_dist[curr] + wt[{curr, c}] < end_dist[c]) && flow[{curr, c}] <= min_flow)
 			{
 				end_dist[c] = end_dist[curr] + wt[{curr, c}];
-				vis.insert({ end_dist[c], c });
+				vis.push({ -end_dist[c], c });
 				par[c] = curr;
 			}
 		}
@@ -76,47 +82,61 @@ bool init_dij(int min_flow, int n)
 
 void main_dij(int min_flow, int n)
 {
-	set<pair<int, pair<int, int> > > vis;
-	vis.insert({ 0, { n, -1 }});
+	priority_queue<pair<int, pair<int, int> > > vis;
+	set<pair<int, int> > used;
+	vis.push({ 0, { n, -1 }});
 
 	if (n == N - 1)
 	{
 		end_dist[n] = 0;
 		while (vis.size())
 		{
-			int curr = vis.begin()->second.first;
-			int prev = vis.begin()->second.second;
-			vis.erase(vis.begin());
-
-			/*
-			if (adj[78].size() > 100)
-				curr = 1273;
-			*/
-			if (curr == 5433)
-				curr = 5433;
+			int curr = vis.top().second.first;
+			int prev = vis.top().second.second;
+			vis.pop();
 
 			for (int c : adj[curr])
 			{
 				if (c == par[curr]) continue;
 				if (flow[{curr, c}] > min_flow) continue;
 
-				if (end_dist[c] == 33686018 || end_dist[curr] + wt[{curr, c}] < end_dist[c])
+				if (end_dist[c] == MAX_INT || end_dist[curr] + wt[{curr, c}] < end_dist[c])
 				{
 					if (prev == -1 && !is_path[{curr, c}])
 					{
-						rfirst[ind[{curr, c}]] = path.size();
-						rfirst[ind[{c, curr}]] = path.size();
+						rfirst[ind[{curr, c}]] = path.size() + 1;
+						rfirst[ind[{c, curr}]] = path.size() + 1;
+						used.insert({ curr, c });
+						used.insert({ c, curr });
 					}
 					else if (!is_path[{curr, c}])
 					{
-						rfirst[ind[{curr, c}]] = rfirst[ind[{ prev, curr }]];
-						rfirst[ind[{c, curr}]] = rfirst[ind[{ prev, curr }]];
+						rfirst[ind[{curr, c}]] = rfirst[ind[{ prev, curr }]] - is_path[{prev, curr}];
+						rfirst[ind[{c, curr}]] = rfirst[ind[{ prev, curr }]] - is_path[{prev, curr}];
+						used.insert({ curr, c });
+						used.insert({ c, curr });
 					}
 
 					end_dist[c] = end_dist[curr] + wt[{curr, c}];
-					vis.insert({ end_dist[c], { c, curr } });
+					vis.push({ -end_dist[c], { c, curr } });
 					par[c] = curr;
 				}
+
+				if (used.find({ c, curr }) == used.end())
+					if (prev == -1 && !is_path[{curr, c}])
+					{
+						rfirst[ind[{curr, c}]] = path.size() + 1;
+						rfirst[ind[{c, curr}]] = path.size() + 1;
+						used.insert({ curr, c });
+						used.insert({ c, curr });
+					}
+					else if (!is_path[{curr, c}])
+					{
+						rfirst[ind[{curr, c}]] = rfirst[ind[{ prev, curr }]] - is_path[{prev, curr}];
+						rfirst[ind[{c, curr}]] = rfirst[ind[{ prev, curr }]] - is_path[{prev, curr}];
+						used.insert({ curr, c });
+						used.insert({ c, curr });
+					}
 			}
 		}
 	}
@@ -125,31 +145,53 @@ void main_dij(int min_flow, int n)
 		dist[n] = 0;
 		while (vis.size())
 		{
-			int curr = vis.begin()->second.first;
-			int prev = vis.begin()->second.second;
-			vis.erase(vis.begin());
+			int curr = vis.top().second.first;
+			int prev = vis.top().second.second;
+			vis.pop();
 
 			for (int c : adj[curr])
 			{
+				if (curr == 5 && c == 3)
+					dist[0] = 0;
 				if (c == npar[curr]) continue;
 				if (flow[{curr, c}] > min_flow) continue;
 
-				if ((dist[c] == 33686018 || dist[curr] + wt[{curr, c}] < dist[c]))
+				if ((dist[c] == MAX_INT || dist[curr] + wt[{curr, c}] < dist[c]))
 				{
 					if (prev == -1 && !is_path[{curr, c}])
 					{
-						llast[ind[{curr, c}]] = -1;
-						llast[ind[{c, curr}]] = -1;
+						llast[ind[{curr, c}]] = 0;
+						llast[ind[{c, curr}]] = 0;
+						used.insert({ curr, c });
+						used.insert({ c, curr });
 					}
 					else if (!is_path[{curr, c}])
 					{
-						llast[ind[{curr, c}]] = llast[ind[{ prev, curr }]];
-						llast[ind[{c, curr}]] = llast[ind[{ prev, curr }]];
+						llast[ind[{curr, c}]] = llast[ind[{ prev, curr }]] + is_path[{prev, curr}];
+						llast[ind[{c, curr}]] = llast[ind[{ prev, curr }]] + is_path[{prev, curr}];
+						used.insert({ curr, c });
+						used.insert({ c, curr });
 					}
 					dist[c] = dist[curr] + wt[{curr, c}];
-					vis.insert({ dist[c], { c, curr } });
+					vis.push({ -dist[c], { c, curr } });
 					npar[curr] = c;
 				}
+
+				if (used.find({ c, curr }) == used.end())
+					if (prev == -1 && !is_path[{curr, c}])
+					{
+						llast[ind[{curr, c}]] = 0;
+						llast[ind[{c, curr}]] = 0;
+						used.insert({ curr, c });
+						used.insert({ c, curr });
+					}
+					else if (!is_path[{curr, c}])
+					{
+						llast[ind[{curr, c}]] = llast[ind[{ prev, curr }]] - is_path[{prev, curr}];
+						llast[ind[{c, curr}]] = llast[ind[{ prev, curr }]] - is_path[{prev, curr}];
+						used.insert({ curr, c });
+						used.insert({ c, curr });
+					}
 			}
 		}
 	}
@@ -158,6 +200,8 @@ void main_dij(int min_flow, int n)
 int main() {
 	for (int t = 1; t <= 10; t++)
 	{
+		ofstream cout(to_string(t) + ".out");
+		ifstream cin(to_string(t) + ".in");
 		edges.clear();
 		path.clear();
 		wt.clear();
@@ -166,8 +210,6 @@ int main() {
 		memset(llast, 0, sizeof llast);
 		memset(rfirst, 0, sizeof rfirst);
 		for (int i = 0; i < MAXN; i++) adj[i].clear();
-		ifstream cin(to_string(t) + ".in");
-		ofstream cout(to_string(t) + ".out");
 
 		cin >> N >> M >> K;
 
@@ -195,6 +237,8 @@ int main() {
 		int hi = 1e9;
 		while (lo + 1 < hi)
 		{
+			memset(llast, 0, sizeof llast);
+			memset(rfirst, 0, sizeof rfirst);
 			int m = (lo + hi) / 2;
 
 			//reset everything
@@ -202,9 +246,9 @@ int main() {
 			is_path.clear();
 			memset(par, -1, sizeof par);
 			memset(npar, -1, sizeof npar);
-			memset(end_dist, 2, sizeof end_dist);
-			memset(dist, 2, sizeof dist);
-			memset(st, 2, sizeof st);
+			fill(end_dist, end_dist + MAXN, MAX_INT);
+			fill(dist, dist + MAXN, MAX_INT);
+			fill(st, st + 4 * MAXN, MAX_INT);
 
 			if (!init_dij(m, N - 1))
 			{
@@ -223,27 +267,30 @@ int main() {
 				is_path[{ prev, n }] = true;
 				is_path[{ n, prev }] = true;
 
-				llast[ind[{n, prev}]] = path.size() - 2;
-				llast[ind[{prev, n}]] = path.size() - 2;
-				rfirst[ind[{n, prev}]] = path.size() - 1;
-				rfirst[ind[{prev, n}]] = path.size() - 1;
+				llast[ind[{n, prev}]] = path.size();
+				llast[ind[{prev, n}]] = path.size();
+				rfirst[ind[{n, prev}]] = path.size();
+				rfirst[ind[{prev, n}]] = path.size();
 			}
 
 			memset(par, -1, sizeof par);
 			memset(npar, -1, sizeof npar);
-			memset(end_dist, 2, sizeof end_dist);
-			memset(dist, 2, sizeof dist);
-			memset(llast, 0, sizeof llast);
-			memset(rfirst, 0, sizeof rfirst);
+			fill(end_dist, end_dist + MAXN, MAX_INT);
+			fill(dist, dist + MAXN, MAX_INT);
 			main_dij(m, N - 1);
 			main_dij(m, 0);
 
 			for (int i = 0; i < edges.size(); i++)
-				update(0, N - 1, llast[i] + 1, rfirst[i], 0, dist[edges[i].first] + wt[edges[i]] + end_dist[edges[i].second]);
+			{
+				if (flow[edges[i]] > m || is_path[edges[i]]) continue;
+				if (m == 531250000)
+					dist[0] = 0;
+				update(0, path.size() + 1, llast[i], rfirst[i], 0, wt[edges[i]] + min(dist[edges[i].first]  + end_dist[edges[i].second], dist[edges[i].second] + end_dist[edges[i].first]));
+			}
 
 			int bin_ans = 0;
 			for (int i = 0; i < path.size(); i++)
-				bin_ans = max(bin_ans, query(0, N - 1, 0, i));
+				bin_ans = max(bin_ans, query(0, path.size() + 1, 0, i + 1));
 
 			if (bin_ans > K)
 				lo = m;
