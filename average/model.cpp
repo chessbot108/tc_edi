57471bdc3218f42d5c31590f0ce8fd9fcf33c097
Bossologist <85655686+Bossologist@users.noreply.github.com>
2021-08-02 08:33:04 -0700

    Update model.cpp

--- average/model.cpp
+++ average/model.cpp
@@ -5,25 +5,19 @@
 using namespace std;
 
 int main() {
-	for (int t = 1; t <= 10; t++)
+	int N;
+	cin >> N;
+
+	int sum = 0;
+	int mini = 1000000009;
+	for (int i = 0; i < N; i++)
 	{
-		ifstream cin(to_string(t) + ".in");
-		ofstream cout(to_string(t) + ".out");
+		int a;
+		cin >> a;
 
-		int N;
-		cin >> N;
-
-		int sum = 0;
-		int mini = 1000000009;
-		for (int i = 0; i < N; i++)
-		{
-			int a;
-			cin >> a;
-
-			mini = min(mini, 10 * a);
-			sum += 10 * a;
-		}
-
-		cout << (double)((sum - mini) / (N - 1)) / 10 << "\n";
+		mini = min(mini, 10 * a); // we do 10*a to get tenth
+		sum += 10 * a; // multiply by 10 to round down to tenth
 	}
+
+	cout << ((sum - mini) / (N - 1)) / 10 << "." << ((sum - mini) / (N - 1)) % 10 << "\n";
 }
