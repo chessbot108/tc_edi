3a4ce36749ad2e79db0a93a8aa67493eb8efcd5f
Bossologist <85655686+Bossologist@users.noreply.github.com>
2021-08-02 08:37:09 -0700

    Update model.cpp

--- abc's/easy/model.cpp
+++ abc's/easy/model.cpp
@@ -1,9 +1,3 @@
-/*
-* a game where you play A, B, C
-* player knows oppenets moves, gets a point everytime plays a card the opponent has placed at least the most(ties included)
-* tries to get most points
-*/
-
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -11,36 +5,30 @@
 using namespace std;
 #define MAXN 100001
 
-//cant switch cards, must choose only one to play O(N) very easy
 int main() {
-	for (int t = 1; t <= 10; t++)
+	int N;
+	cin >> N;
+
+	string s;
+	cin >> s;
+
+	int a = 0, b = 0, c = 0;
+	int ansa = 0, ansb = 0, ansc = 0;
+	for (int i = 0; i < N; i++)
 	{
-		ofstream cout(to_string(t) + ".out");
-		ifstream cin(to_string(t) + ".in");
+		//update the frequency of each letter up to this point
+		if (s[i] == 'A') a++;
+		if (s[i] == 'B') b++;
+		if (s[i] == 'C') c++;
 
-		int N;
-		cin >> N;
+		int max_used = max(max(a, b), c);
 
-		string s;
-		cin >> s;
-
-		int a = 0, b = 0, c = 0;
-		int ansa = 0, ansb = 0, ansc = 0;
-		for (int i = 0; i < N; i++)
-		{
-			if (s[i] == 'A') a++;
-			if (s[i] == 'B') b++;
-			if (s[i] == 'C') c++;
-
-			int max_used = max(max(a, b), c);
-
-			if (a == max_used) ansa++;
-			if (b == max_used) ansb++;
-			if (c == max_used) ansc++;
-		}
-
-		cout << max(max(ansa, ansb), ansc) << "\n";
+		//update how many points each letter would get if that was the only letter they played
+		if (a == max_used) ansa++;
+		if (b == max_used) ansb++;
+		if (c == max_used) ansc++;
 	}
+
+	cout << max(max(ansa, ansb), ansc) << "\n";
 	return 0;
 }
-
